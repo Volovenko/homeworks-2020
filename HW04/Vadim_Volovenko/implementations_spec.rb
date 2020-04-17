1e4bb030b5fd2d048e5732298b7284ee124bbdae
Volovenko <vadim.volovenko@gmail.com>
2020-04-17 09:39:55 +0300

    fix for rubocop

--- HW04/Vadim_Volovenko/implementations_spec.rb
+++ HW04/Vadim_Volovenko/implementations_spec.rb
@@ -1,48 +1,51 @@
+# rubocop:disable Metrics/BlockLength
 require 'rspec'
 require_relative 'implementations.rb'
 
-describe '#valid?' do
-  subject { Implementations.new([1, 2, 3]) }
-  context 'when array is not empty' do
-    it 'array valid' do
-      expect(subject.valid?).to eq true
-    end
-  end
+RSpec.describe Implementations do
+  subject { described_class.new([1, 2, 3]) }
 
-  context 'when array empty' do
-    subject { Implementations.new([]) }
-    it 'array nill' do
-      expect(subject.valid?).to eq false
-    end
-  end
-end
-
-describe '#my_map' do
-  subject { Implementations.new([1, 2, 3]) }
-  context 'when array valid' do
-    it 'should return right array' do
-      expect(subject.my_map { |element| element + 1 }).to eq [2, 3, 4]
-    end
-  end
-end
-
-describe '#my_select' do
-  subject { Implementations.new([1, 2, 3]) }
-  context 'when array valid' do
-    it 'should return right array' do
-      expect(subject.my_select { |element| element > 2 }).to eq [3]
-    end
-  end
-end
-
-describe '#my_each' do
-  subject { Implementations.new([1, 2, 3]) }
-  context 'when strings valid' do
-    it 'should return right string' do
-      expectation = expect do
-        (subject.my_each { |element| puts element * 2 })
+  describe '#valid?' do
+    context 'when array is not empty' do
+      it 'array valid' do
+        expect(subject.valid?).to eq true
       end
-      expectation.to output("2\n4\n6\n").to_stdout
+    end
+
+    context 'when array empty' do
+      subject { described_class.new([]) }
+
+      it 'array nill' do
+        expect(subject.valid?).to eq false
+      end
+    end
+  end
+
+  describe '#my_map' do
+    context 'when array valid' do
+      it 'return right array' do
+        expect(subject.my_map { |element| element + 1 }).to eq [2, 3, 4]
+      end
+    end
+  end
+
+  describe '#my_select' do
+    context 'when array valid' do
+      it 'return right array' do
+        expect(subject.my_select { |element| element > 2 }).to eq [3]
+      end
+    end
+  end
+
+  describe '#my_each' do
+    context 'when strings valid' do
+      it 'return right string' do
+        expectation = expect do
+          (subject.my_each { |element| puts element * 2 })
+        end
+        expectation.to output("2\n4\n6\n").to_stdout
+      end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
